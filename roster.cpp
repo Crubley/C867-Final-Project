8660a1b61721636142c69236da4c62d17ee305f3
Dan Bowering <danbowering@gmail.com>
2020-09-16 12:10:47 -0400

    Add files via upload

--- roster.cpp
+++ roster.cpp
@@ -48,34 +48,56 @@ void Roster::parse(string studentData)
 
 	DegreeProgram dp;
 	if (degree.substr() == "SECURITY") {	// Check the string to determine degeree program
-		DegreeProgram dp = SECURITY;
+		dp = SECURITY;
 	}
 	else if (degree.substr() == "NETWORK") {
-		DegreeProgram dp = NETWORK;
+		dp = NETWORK;
 	}
 	else if (degree.substr() == "SOFTWARE") {
-		DegreeProgram dp = SOFTWARE;
+		dp = SOFTWARE;
 	}
 
 	// Add parsed data to row
-	add(sID, fn, ln, em, age, nod1, nod1, nod3, dp);
+	add(sID, fn, ln, em, age, nod1, nod2, nod3, dp);
 }
 
 // Add parsed data to row 
 void Roster::add(string sID, string sFirstName, string sLastName, string sEmail, int sAge, double sDays1, double sDays2, double sDays3, DegreeProgram dp)
 {
 	// Array to hold number of days 
-	double daysArray[3] = {sDays1, sDays2, sDays3};
+	double daysArray[3] = {static_cast<double>(sDays1), static_cast<double>(sDays2), static_cast<double>(sDays3)};
 
 	// Make the new Student object
 	classRosterArray[++lastIndex] = new Student(sID, sFirstName, sLastName, sEmail, sAge, daysArray, dp);
 }
 
-// Remove method - Removes students by student ID. Prints error message if student ID is not found.
+// Removes students by student ID. Prints error message if student ID is not found.
 void Roster::remove(string studentID)
-{}
+{
+	bool found = false;
+	for (int i = 0; i <= Roster::lastIndex; i++)
+	{
+		if (classRosterArray[i]->getID() == studentID)
+		{
+			found = true;
+			if (i < numStudents - 1)
+			{
+				Student* temp = classRosterArray[i];
+				classRosterArray[i] = classRosterArray[numStudents - 1];
+				classRosterArray[numStudents - 1] = temp;
+			}
+			Roster::lastIndex--;
+		}
+	}
+	if (found)
+	{
+		cout << "The student with Student ID: " << studentID << " has been removed from the roster." << endl;
+	}
+	else
+		cout << "student ID: " << studentID << " was not found." << endl;
+}
 
-// Print all method
+// Prints all student data
 void Roster::printAll()
 {
 	for (int i = 0; i <= Roster::lastIndex; i++)
@@ -85,15 +107,14 @@ void Roster::printAll()
 		cout << classRosterArray[i]->getLastName() << '\t';
 		cout << classRosterArray[i]->getEmail() << '\t';
 		cout << classRosterArray[i]->getAge() << '\t';
-		cout << classRosterArray[i]->getNumDays()[0] << ',';
-		cout << classRosterArray[i]->getNumDays()[1] << ',';
-		cout << classRosterArray[i]->getNumDays()[2] << ',';
-		cout << classRosterArray[i]->getNumDays()[3] << '\t';
+		cout << classRosterArray[i]->getNumDays()[0] << ", ";
+		cout << classRosterArray[i]->getNumDays()[1] << ", ";
+		cout << classRosterArray[i]->getNumDays()[2] << '\t';
 		cout << degreeProgramStrings[classRosterArray[i]->getDegreeProgram()] << endl;
 	}
 }
 
-// Print by degree program method
+// Prints student data by a specified degree program 
 void Roster::printByDegreeProgram(DegreeProgram dp)
 {
 	for (int i = 0; i <= Roster::lastIndex; i++) 
@@ -108,20 +129,23 @@ void Roster::printAverageDaysInCourse(string studentID)
 {
 	for (int i = 0; i <= Roster::lastIndex; i++)
 	{
-		cout << "Student ID: " << classRosterArray[i]->getID();
-		double average = (classRosterArray[i]->getNumDays()[0] + classRosterArray[i]->getNumDays()[1] + classRosterArray[i]->getNumDays()[2]) / 3.0;
-		cout << " averages " << average << " days per course." << endl;
+		if (this->classRosterArray[i]->getID() == studentID)
+		{
+			cout << "Student ID: " << classRosterArray[i]->getID();
+			double average = (classRosterArray[i]->getNumDays()[0] + classRosterArray[i]->getNumDays()[1] + classRosterArray[i]->getNumDays()[2]) / 3.0;
+			cout << " averages " << average << " days per course." << endl;
+		}
 	}
 }
 
-// Print invalid email addresses method
+// Finds and prints any invalid email addresses 
 void Roster::printInvalidEmails()
 {
 	bool invalidEmail = false;
 	for (int i = 0; i <= Roster::lastIndex; i++)
 	{
 		string em = (classRosterArray[i]->getEmail());
-		if (em.find('@') == string::npos && (em.find('.') == string::npos) || (em.find(' ') != string::npos))
+		if (em.find('@') == string::npos || (em.find('.') == string::npos) || (em.find(' ') != string::npos))
 		{
 			invalidEmail = true;
 			cout << "The following email addresses are invalid: " << classRosterArray[i]->getEmail() << endl;
