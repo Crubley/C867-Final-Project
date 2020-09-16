c8c0a5a47e93971a0a816841be95482ace84638a
Dan Bowering <danbowering@gmail.com>
2020-09-16 12:10:23 -0400

    Add files via upload

--- degree.h
+++ degree.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <string>
 
-enum DegreeProgram {UNKNOWN, SECURITY, NETWORK, SOFTWARE};	// Enumerated data type for various program degrees
+// Enumerated data type for various program degrees
+enum DegreeProgram {UNKNOWN, SECURITY, NETWORK, SOFTWARE};	
 
-static const std::string degreeProgramStrings[] = {"UNKNOWN", "SECURITY", "NETWORK", "SOFTWARE"};		// array of strings to represent the degree program index
+// Array of strings to represent the degree program index
+static const std::string degreeProgramStrings[] = {"UNKNOWN", "SECURITY", "NETWORK", "SOFTWARE"};		
